a8dd2fdd9c701a4496ebd843a71b1d823534c9c2
Wusi-Jeong <jws9595@naver.com>
2022-09-14 18:37:00 +0900

    2022. 09. 14

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,25 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
     return (
-      <div className='bg-slate-400 py-20 px-20 grid gap-10'>
-        <div className='bg-white p-6 rounded-3xl shadow-xl'>
-          <span className='font-semibold text-3xl'>Select Item</span>
-          <div className='flex justify-between my-2'>
-            <span className='text-gray-500 font-semibold'>Grey Chair</span>
-            <span className='font-semibold'>$19</span>
-          </div>
-          <div className='flex justify-between'>
-            <span className='text-gray-500 font-semibold'>Grey Chair</span>
-            <span className='font-semibold'>$19</span>
-          </div>
+      <div className='bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen'>
+        <div className='bg-white dark:bg-black flex flex-col justify-between p-6 rounded-3xl shadow-xl'>
+          <span className='font-semibold dark:text-white text-3xl'>Select Item</span>
+          <ul>
+            {[1, 2].map((i) => (
+              <div className='flex justify-between my-2 '>
+              <span className='text-gray-500 dark:text-gray-100 font-semibold'>Grey Chair</span>
+              <span className='font-semibold dark:text-white'>$19</span>
+            </div>
+            ))}
+          </ul>
           <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed font-semibold'>
             <span>Total</span>
             <span className='font-semibold'>$10</span>
           </div>
-          <div className='mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto'>Checkout</div>
+          <button className='mt-5 bg-blue-500 dark:bg-black text-white p-3 text-center bor rounded-xl w-3/4 mx-auto dark:border-white dark:border dark:hover:bg-white hover:bg-teal-500 dark:hover:text-black active:bg-yellow-500 focus:text-red-500'>Checkout</button>
         </div>
-        <div className='bg-white overflow-hidden rounded-3xl shadow-xl'>
-          <div className='bg-blue-500 p-6 pb-14'><span className='text-white text-2xl'>Profile</span>
+        <div className='bg-white overflow-hidden rounded-3xl shadow-xl group'>
+          <div className='portrait:bg-indigo-600 landscape:bg-teal-500 p-6 pb-14 xl:pb-40'><span className='text-white text-2xl'>Profile</span>
           </div>
           <div className='rounded-3xl p-6 bg-white relative -top-5'>
             <div className='flex relative -top-16  items-end justify-between'>
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
                 <span className='text-sm text-gray-500'>Orders</span>
                 <span className='font-medium'>340</span>
               </div>
-            <div className='h-24 w-24 bg-red-400 rounded-full'/>
+            <div className='h-24 w-24 bg-red-500 rounded-full group-hover:bg-red-300 transition-colors'/>
             <div className='flex flex-col items-center'>
                 <span className='text-sm text-gray-500'>Orders</span>
                 <span className='font-medium'>$340</span>
@@ -40,11 +40,69 @@ const Home: NextPage = () => {
               </div>
           </div>
         </div>
-        <div className='bg-white p-6 rounded-3xl shadow-xl'></div>
-        <div className='bg-white p-6 rounded-3xl shadow-xl'></div>
+        <div className='bg-white p-6 rounded-3xl shadow-xl lg:col-span-2 xl:col-span-1'>
+          <div className='flex mb-5 justify-between items-center'>
+            <span>←</span>
+            <div className='space-x-3'>
+              <span>⭐️ 4.9</span>
+              <span className='shadow-xl p-2 rounded-md'>❤️</span>
+            </div>
+          </div>
+          <div className='bg-zinc-400 h-72 mb-5'/>
+          <div className='flex flex-col'>
+            <span className='font-medium text-xl'>Swoon Lounge</span>
+            <span className='text-xs text-gray-500'>Chair</span>
+            <div className='mt-3 mb-5 flex justify-between items-center'>
+            <div className='space-x-2'>
+              <button className='w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 focus:ring-offset-2 ring-yellow-500 transition' />
+              <button className='w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 focus:ring-offset-2 ring-indigo-500 transition' />
+              <button className='w-5 h-5 rounded-full bg-teal-500 focus:ring-2 focus:ring-offset-2 ring-teal-500 transition' />
+            </div>
+            <div className='flex items-center space-x-5'>
+            <button className='rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500'>-</button>
+            <span>1</span>
+            <button className='rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500'>+</button>
+            </div>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span className='font-medium text-2xl'>$450</span>
+            <button className='bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-lg'>Add to cart</button>
+          </div>
+        </div>
+      </div>
       </div>
 
     );
 };
+
+// const Home: NextPage = () => {
+//   return (
+//     <form className='flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-100'>
+//       <input type='text' required placeholder='Username' className='required:bg-yellow-500 invalid:bg-red-500 valid:bg-teal-500' />
+//       <input type='password' required placeholder='Password' />
+//       <input type='submit' value='Login' className='bg-white' />
+//     </form>
+//   )
+// }
+
+// const Home: NextPage = () => {
+//   return (
+//     <form className='flex flex-col space-y-2 p-5 '>
+//       <input type='text' required placeholder='Username' className='border p-1 peer border-gray-400' />
+//       <span className='hidden peer-invalid:block peer-invalid:text-red-500'>This input is invalid</span>
+//       <span className='hidden peer-valid:block peer-valid:text-teal-500'>This input is invalid</span>
+//       <span className='hidden peer-hover:block peer-valid:text-amber-500'>Hello</span>
+//       <input type='submit' value='Login' className='bg-white' />
+//     </form>
+//   )
+// }
+
+// const Home: NextPage = () => {
+//   return (
+//     <div className='flex flex-col space-y-2 p-5'>
+//       <p className='first-letter:text-7xl first-letter:hover:text-purple-400'>hello</p>
+//     </div>
+//   )
+// }
 
 export default Home
